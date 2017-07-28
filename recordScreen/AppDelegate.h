5adc133b54cec31596e7cc191d26926dd6436c1c
guoyf <guoyf@mapbar.com>
2017-07-28 10:54:24 +0800

    Initial Commit

---
+++ recordScreen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  recordScreen
+//
+//  Created by guoyf on 2017/7/28.
+//  Copyright © 2017年 guoyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
